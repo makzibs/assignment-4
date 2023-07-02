ba58ddb6fbee1c01a17618e251a83c01c68f980e
Anil Khanal <66034315+makzibs@users.noreply.github.com>
2023-07-02 23:27:08 +0300

    5th commit

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ const app = express();
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  // Retrieve the food parameter from the URL
- res.send("the frontend is running too ")
-});
+
 
 // Define the GET route for retrieving recipes
 app.get('/recipe/:food', (req, res) => {
@@ -23,9 +20,28 @@ app.get('/recipe/:food', (req, res) => {
   };
 
   // Send the recipe as a JSON response
-  //res.send(recipe);
-  res.render('index', { recipe });
+  //res.render('index', {recipe});
   res.send(recipe);
+  console.log(recipe);
+  
+  
+});
+
+app.get('/', (req, res) => {
+  // Retrieve the food parameter from the URL
+  
+
+  // Define the recipe object
+  const recipe = {
+    name: "Pizza",
+    instructions: ['Step 1', 'Step 2', 'Step 3'],
+    ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3']
+  };
+
+  // Send the recipe as a JSON response
+  
+  res.render('index', { recipe });
+  
 });
 
 // Start the server
